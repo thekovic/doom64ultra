0df3eafdf9639c056c9b0c9454720bfbb802fa6a
azamorapl <adolfo.zamorapl@gmail.com>
2023-06-06 03:09:24 -0500

    Allow encoding passwords for levels 0 and 1
    Matches behavior with 2020 remaster and EX

--- doom64/src/in_main.c
+++ doom64/src/in_main.c
@@ -135,7 +135,7 @@ void IN_Start(void) // 80004AF0
 	last_ticon = 0;
 	text_alpha = 255;
 
-    if ((nextmap >= 2) && (nextmap < LASTLEVEL))
+    if ((nextmap >= 0) && (nextmap < LASTLEVEL))
 	{
 		M_EncodePassword(Passwordbuff);
         CurPasswordSlot = 16;
@@ -148,7 +148,7 @@ void IN_Stop(void) // 80004DB0
 {
 	S_StopMusic();
 
-    if ((nextmap >= 2) && (nextmap < LASTLEVEL))
+    if ((nextmap >= 0) && (nextmap < LASTLEVEL))
     {
         if (EnableExpPak) {
             MiniLoop(M_SavePakStart,M_SavePakStop,M_SavePakTicker,M_SavePakDrawer);
