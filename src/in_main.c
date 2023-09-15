e2be20a1428313e924ccea9f1c90375776885074
nova <136651697+9nova@users.noreply.github.com>
2023-09-15 17:22:29 -0400

    fix crash after save screen

--- src/in_main.c
+++ src/in_main.c
@@ -181,6 +181,7 @@ void IN_Stop(void) // 80004DB0
                 MiniLoop(M_SavePakStart,M_SavePakStop,M_SavePakTicker,M_MenuGameDrawer);
             }
         }
+        MenuCall = NULL;
     }
 
     I_WIPE_FadeOutScreen();
